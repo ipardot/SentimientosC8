8b8860ff5fb72bda93d6a568a19bfc284003f062
Isabella Pardo <ipardot@eafit.edu.co>
2026-09-10 13:17:06 -0500

    Load animations based on sentiment analysis

--- app.py
+++ app.py
@@ -37,8 +37,16 @@ with st.expander('Analizar texto'):
         x=round(blob.sentiment.polarity,2)
         if x > 0.0 and x <=1.0:
             st.write( 'Es un sentimiento Positivo 😊')
-            
+            with open ('happyP.json') as source:
+              animation = json.load (source)
+            st.lottie(animation, width=350)
         elif x >=-1 and x <= 0:
             st.write( 'Es un sentimiento Negativo 😔')
+            with open ('tristeM.json') as source:
+              animation = json.load (source)
+            st.lottie(animation, width=350)          
         else:
             st.write( 'Es un sentimiento Neutral 😐')
+            with open ('NeutralE.json') as source:
+              animation = json.load (source)
+            st.lottie(animation, width=350) 
